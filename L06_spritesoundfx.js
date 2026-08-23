4b6999c3fdfb80d942bfafe531d4cdf833f9da22
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-23 08:27:35 +0000

    Aug 23, 2026, 8:27 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -2,7 +2,7 @@ let x = 200;
 let y = 200;
 function setup(){
     createCanvas(400,400);
-    background(220);
+    bgMusic.loop();
 }
 function preload(){
     soundEffect = loadSound('assets/pop.mp3');
