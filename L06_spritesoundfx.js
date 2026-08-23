4cf3b050f43d69bf14781f1a3f76394ef7a9c5ea
Computhink-CS142 <cs142@computhink.com.sg>
2026-08-23 09:04:05 +0000

    Aug 23, 2026, 9:04 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -35,9 +35,9 @@ function draw(){
     if(staticImageY==0){
         background("red")
     soundEffect.play}
-    if(staticImageY==400)
+    if(staticImageY==400){
         background("red")
-    soundEffect.play
+    soundEffect.play}
     staticImageX = constrain(staticImageX,0,400);
     staticImageY = constrain(staticImageY,0,400);
     image(staticImage,staticImageX,staticImageY,50,50);
